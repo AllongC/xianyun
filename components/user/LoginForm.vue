690b5b9dc3c89989e417a86f87651ca4b690d4e5
杨腾祺 <3150475761@qq.com>
2020-07-12 15:44:06 +0800

    05-登录页面

---
+++ components/user/LoginForm.vue
@@ -0,0 +1,89 @@
+<template>
+  <el-form :model="form" ref="form" :rules="rules" class="form">
+    <el-form-item class="form-item" prop="username">
+      <el-input placeholder="用户名/手机" v-model="form.username" @focus="clearErr('username')"></el-input>
+    </el-form-item>
+
+    <el-form-item class="form-item" prop="password">
+      <el-input
+        placeholder="密码"
+        type="password"
+        v-model="form.password"
+        @focus="clearErr('password')"
+      ></el-input>
+    </el-form-item>
+
+    <p class="form-text">
+      <nuxt-link to="#">忘记密码</nuxt-link>
+    </p>
+
+    <el-button class="submit" type="primary" @click="handleLoginSubmit">登录</el-button>
+  </el-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 5, max: 11, message: "请输入5-11位数字", trigger: "blur" },
+          {
+            pattern: /^d+$/,
+            message: "请输入正确的格式",
+            trigger: "blur"
+          }
+        ],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }]
+      }
+    };
+  },
+
+  methods: {
+    clearErr(val) {
+      this.$refs.form.clearValidate(val);
+    },
+    handleLoginSubmit() {
+      this.$refs.form.validate(status, res => {
+        if (status) {
+          this.$axios({
+            url: "/accounts/login",
+            method: "post",
+            data: this.form
+          }).then(res => {
+            const { data } = res;
+            console.log(data);
+          });
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.form {
+  padding: 25px;
+}
+
+.form-item {
+  margin-bottom: 20px;
+}
+
+.form-text {
+  font-size: 12px;
+  color: #409eff;
+  text-align: right;
+  line-height: 1;
+}
+
+.submit {
+  width: 100%;
+  margin-top: 10px;
+}
+</style>
