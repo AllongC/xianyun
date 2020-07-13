b0cc2f17cef7af581c08ec9222cc542fbb9a06a3
杨腾祺 <3150475761@qq.com>
2020-07-13 15:18:47 +0800

    08-localStorage插件

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,11 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '@/plugins/element-ui'
+    '@/plugins/element-ui',
+    {
+      src: '@/plugins/localStorage',
+      ssr: false
+    }
   ],
 
   /*
