3d52f1430c3dba90f9850ae2eaea878973ede2f6
杨腾祺 <3150475761@qq.com>
2020-07-16 17:08:41 +0800

    24-机票列表渲染

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,20 +1,65 @@
 <template>
-  <div>机票</div>
+  <section class="contianer">
+    <el-row type="flex" justify="space-between">
+      <!-- 顶部过滤列表 -->
+      <div class="flights-content">
+        <!-- 过滤条件 -->
+        <div></div>
+
+        <!-- 航班头部布局 -->
+        <FlightsListHead />
+
+        <!-- 航班信息 -->
+        <div>
+          <FlightsItem v-for="(item,index) in flights" :key="index" :flights="item" />
+        </div>
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside">
+        <!-- 侧边栏组件 -->
+      </div>
+    </el-row>
+  </section>
 </template>
 
 <script>
+import FlightsListHead from "@/components/air/FlightsListHead";
+import FlightsItem from "@/components/air/FlightsItem";
 export default {
+  components: {
+    FlightsListHead,
+    FlightsItem
+  },
+  data() {
+    return {
+      flights: []
+    };
+  },
   created() {
     this.$axios({
       url: "/airs",
       method: "get",
       params: this.$route.query
     }).then(res => {
-      console.log(res);
+      this.flights = res.data.flights;
     });
   }
 };
 </script>
 
 <style lang="less" scoped>
+.contianer {
+  width: 1000px;
+  margin: 20px auto;
+}
+
+.flights-content {
+  width: 745px;
+  font-size: 14px;
+}
+
+.aside {
+  width: 240px;
+}
 </style>
