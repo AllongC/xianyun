9616b58f72e80a1cfdb36433314a2a4db59c3e23
杨腾祺 <3150475761@qq.com>
2020-07-16 16:23:02 +0800

    23-特价机票

--- pages/air/index.vue
+++ pages/air/index.vue
@@ -40,7 +40,21 @@
     </h2>
 
     <!-- 特价机票 -->
-    <div class="air-sale"></div>
+    <div class="air-sale">
+      <el-row type="flex" class="air-sale-pic" justify="space-between">
+        <el-col :span="6" v-for="(item, index) in sales" :key="index">
+          <nuxt-link
+            :to="`/air/flights?departCity=${item.departCity}&departCode=${item.departCode}&destCity=${item.destCity}&destCode=${item.destCode}&departDate=${item.departDate}`"
+          >
+            <img :src="$axios.defaults.baseURL+item.cover" />
+            <el-row class="layer-bar" type="flex" justify="space-between">
+              <span>{{item.departCity}}-{{item.destCity}}</span>
+              <span>￥699</span>
+            </el-row>
+          </nuxt-link>
+        </el-col>
+      </el-row>
+    </div>
   </section>
 </template>
 
@@ -49,6 +63,20 @@ import searchForm from "@/components/air/searchForm";
 export default {
   components: {
     searchForm
+  },
+  data() {
+    return {
+      sales: [{}]
+    };
+  },
+  mounted() {
+    this.$axios({
+      url: "/airs/sale",
+      method: "get"
+    }).then(res => {
+      const { data } = res.data;
+      this.sales = data;
+    });
   }
 };
 </script>
