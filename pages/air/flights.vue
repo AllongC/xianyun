eea6880a9299f145f28d796ad65228c2e326f456
杨腾祺 <3150475761@qq.com>
2020-07-17 16:09:20 +0800

    29-无数据状态

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -10,18 +10,19 @@
         <FlightsListHead />
 
         <!-- 航班信息 -->
-        <div>
-          <FlightsItem v-for="(item,index) in currentFlights" :key="index" :flights="item" />
-          <el-pagination
-            @size-change="changeSize"
-            @current-change="changePage"
-            :current-page="currentPage"
-            :page-sizes="[5,10,15,20]"
-            :page-size="pageSize"
-            layout="total, sizes, prev, pager, next, jumper"
-            :total="total"
-          ></el-pagination>
+        <div v-if="dateList.length">
+          <FlightsItem v-for="(item,index) in dateList" :key="index" :flights="item" />
         </div>
+        <div v-if="!dateList.length" class="none">暂无该航班机票</div>
+        <el-pagination
+          @size-change="changeSize"
+          @current-change="changePage"
+          :current-page="currentPage"
+          :page-sizes="[5,10,15,20]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+        ></el-pagination>
       </div>
 
       <!-- 侧边栏 -->
@@ -36,32 +37,33 @@
 import FlightsListHead from "@/components/air/FlightsListHead";
 import FlightsItem from "@/components/air/FlightsItem";
 export default {
+  components: {
+    FlightsListHead,
+    FlightsItem
+  },
   data() {
     return {
       flights: [],
-      currentFlights: [],
+      dateList: [],
       total: 0,
       pageSize: 5,
       currentPage: 1
     };
   },
   methods: {
-    changePage(page) {
-      this.currentPage = page;
+    currentList() {
       const start = this.pageSize * (this.currentPage - 1);
       const end = this.pageSize * this.currentPage;
-      this.currentFlights = this.flights.slice(start, end);
+      this.dateList = this.flights.slice(start, end);
+    },
+    changePage(page) {
+      this.currentPage = page;
+      this.currentList();
     },
     changeSize(size) {
       this.pageSize = size;
-      const start = this.pageSize * (this.currentPage - 1);
-      const end = this.pageSize * this.currentPage;
-      this.currentFlights = this.flights.slice(start, end);
+      this.currentList();
     }
-  },
-  components: {
-    FlightsListHead,
-    FlightsItem
   },
   created() {
     this.$axios({
@@ -71,9 +73,7 @@ export default {
     }).then(res => {
       this.total = res.data.total;
       this.flights = res.data.flights;
-      const start = this.pageSize * (this.currentPage - 1);
-      const end = this.pageSize * this.currentPage;
-      this.currentFlights = this.flights.slice(start, end);
+      this.currentList();
     });
   }
 };
@@ -92,5 +92,11 @@ export default {
 
 .aside {
   width: 240px;
+}
+.none {
+  height: 300px;
+  line-height: 300px;
+  text-align: center;
+  color: #666;
 }
 </style>
