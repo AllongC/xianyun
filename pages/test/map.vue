af37f498cccf029700273f9112ccdfc3b0410012
杨腾祺 <3150475761@qq.com>
2020-07-23 16:47:22 +0800

    58-高德地图初步使用（下）

--- pages/test/map.vue
+++ pages/test/map.vue
@@ -1,5 +1,10 @@
 <template>
-  <div id="container"></div>
+  <div id="container">
+    <h4 id="status"></h4>
+    <hr />
+    <p id="result"></p>
+    <hr />
+  </div>
 </template>
 
 <script>
@@ -12,32 +17,39 @@ export default {
       });
       AMap.plugin("AMap.Geolocation", function() {
         var geolocation = new AMap.Geolocation({
-          // 是否使用高精度定位，默认：true
-          enableHighAccuracy: true,
-          // 设置定位超时时间，默认：无穷大
-          timeout: 10000,
-          // 定位按钮的停靠位置的偏移量，默认：Pixel(10, 20)
-          buttonOffset: new AMap.Pixel(10, 20),
-          //  定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
-          zoomToAccuracy: true,
-          //  定位按钮的排放位置,  RB表示右下
-          buttonPosition: "RB"
+          enableHighAccuracy: true, //是否使用高精度定位，默认:true
+          timeout: 10000, //超过10秒后停止定位，默认：5s
+          buttonPosition: "RB", //定位按钮的停靠位置
+          buttonOffset: new AMap.Pixel(10, 20), //定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+          zoomToAccuracy: true //定位成功后是否自动调整地图视野到定位点
         });
-
-        geolocation.getCurrentPosition();
-        AMap.event.addListener(geolocation, "complete", onComplete);
-        AMap.event.addListener(geolocation, "error", onError);
-
-        function onComplete(data) {
-          // data是具体的定位信息
-          console.log(data);
-        }
-
-        function onError(data) {
-          // 定位出错
-          console.log(data);
-        }
+        map.addControl(geolocation);
+        geolocation.getCurrentPosition(function(status, result) {
+          if (status == "complete") {
+            onComplete(result);
+          } else {
+            onError(result);
+          }
+        });
       });
+      //解析定位结果
+      function onComplete(data) {
+        document.getElementById("status").innerHTML = "定位成功";
+        var str = [];
+        str.push("定位结果：" + data.position);
+        str.push("定位类别：" + data.location_type);
+        if (data.accuracy) {
+          str.push("精度：" + data.accuracy + " 米");
+        } //如为IP精确定位结果则没有精度信息
+        str.push("是否经过偏移：" + (data.isConverted ? "是" : "否"));
+        document.getElementById("result").innerHTML = str.join("<br>");
+      }
+      //解析定位错误信息
+      function onError(data) {
+        document.getElementById("status").innerHTML = "定位失败";
+        document.getElementById("result").innerHTML =
+          "失败原因排查信息:" + data.message;
+      }
     };
     const key = "feebdc3cbcd4f4f6dcab6ad5f2428b3e";
     var url = `https://webapi.amap.com/maps?v=1.4.15&key=${key}&callback=onLoad`;
